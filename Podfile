53e6de017f601b919c759939896274380e277145
Munjal <agile_check_in@Munjals-MacBook-Pro.local>
2015-08-25 18:56:55 -0300

    [#00] Added Pods

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '8.0'
+
+inhibit_all_warnings!
+use_frameworks!
+
+link_with 'Trails'
+
+target :Trails do
+    pod 'Fabric', '~> 1.2.8'
+    pod 'Crashlytics', '~> 3.1.0'
+    pod 'SDWebImage', '~>3.7'
+    pod 'SwiftyDropbox', :git => 'git@github.com:dropbox/SwiftyDropbox.git', :tag => '0.2'
+end
---
+++ Trails/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trails
+//
+//  Created by Munjal Budhabhatti on 8/25/15.
+//  Copyright (c) 2015 Bearch Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
